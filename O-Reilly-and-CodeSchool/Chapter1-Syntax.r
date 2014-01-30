e1fe6f5bb2d90e684e1e5bbcb3d27e0ac74be5d1
KKSV ' chasing zeroes ' <ChicagoBostonDeveloper@gmail.com>
2014-01-29 21:00:23 -0500

    Update Chapter1-Syntax.r

--- O-Reilly-and-CodeSchool/Chapter1-Syntax.r
+++ O-Reilly-and-CodeSchool/Chapter1-Syntax.r
@@ -1,5 +1,53 @@
 # Comments are denoted by the octothorpe symbol
 
+# Expressions
+
 # Like python, I guess R is interactive at it's console and interpreted unlike C
 1+1 # yields [1] 2 . [1] indicating the output(?) 2 indicating the result
 
+"Save the tigers!" # yields [1] "Save the tigers"
+
+6*7 # [1] 42
+
+
+# Logic
+
+3 < 4 # [1] TRUE
+
+2 + 2 == 5 # [1] FALSE
+
+T == TRUE # [1] TRUE
+
+
+# Variables
+
+x <- 9 # 9
+
+x/3 # [1] 3
+
+x <- "Save the Siberian Tiger!" # [1] Save the Siberian Tiger
+
+x <- FALSE
+
+
+# Functions
+
+sum(1,2,3) # [1] 6
+
+rep("Tiger", times = 2) # [1] "Tiger" "Tiger" "Tiger"
+
+sqrt(16) # [1] 4
+
+
+# Help
+
+help(sum) # sum is a function, and it's information is output
+
+example(sum) # example of the function sum is shown
+
+
+# Files / Scripts called
+
+list.files() # displays the directory and it's contents
+
+source("filename.R") # calls the R File Script
