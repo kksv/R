19eec7d80b613ff13fa0bc0fec72d08b3cb83331
KKSV ' chasing zeroes ' <ChicagoBostonDeveloper@gmail.com>
2014-01-29 20:52:17 -0500

    Create Chapter1-Syntax.r

---
+++ O-Reilly-and-CodeSchool/Chapter1-Syntax.r
@@ -0,0 +1,5 @@
+# Comments are denoted by the octothorpe symbol
+
+# Like python, I guess R is interactive at it's console and interpreted unlike C
+1+1 # yields [1] 2 . [1] indicating the output(?) 2 indicating the result
+
