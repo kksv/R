b00d155773fe035a6bc01256cce68f4bba2bdff0
KKSV ' chasing zeroes ' <ChicagoBostonDeveloper@gmail.com>
2014-01-31 19:07:51 -0500

    Update Chapter3-Matrices.r

--- O-Reilly-and-CodeSchool/Chapter3-Matrices.r
+++ O-Reilly-and-CodeSchool/Chapter3-Matrices.r
@@ -1,2 +1,10 @@
 # Matrices
 
+matrix("x",3,5) # creates a 3 x 5 matrix with all elements set as "x"
+
+a<-1:15 # for exercise
+
+matrix(a,3,5) # matrix filled with a's values
+
+dim(a) <- c(3,5) # converts 'a' into a 3 x 5 matrix
+
